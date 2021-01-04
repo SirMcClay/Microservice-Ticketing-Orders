b91a55740e5ff29de911bb965d8d79f4bdeaf7e6
sirmcclay <mcclay.design@gmail.com>
2021-01-04 12:24:52 -0300

    Listing all the logic new route has to handle

--- src/routes/new.ts
+++ src/routes/new.ts
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
 import { requireAuth, validateRequest } from '@sirmctickets/commontickets';
 import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -17,6 +19,16 @@ router.post(
 	],
 	validateRequest,
 	async (req: Request, res: Response) => {
+		// Find the ticket the user is trying to order in the database
+
+		// Make sure that this ticket is not already reserved
+
+		// Calculate an expiration date for this order
+
+		// Build the order and save is to the database
+
+		// Publish an event saying that an order was created
+
 		res.send({});
 	}
 );
