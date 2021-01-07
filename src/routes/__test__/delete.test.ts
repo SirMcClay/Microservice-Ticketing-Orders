eb8ddd66a67fe5a6aecf32ae41f71ce9c85b0342
sirmcclay <mcclay.design@gmail.com>
2021-01-07 09:33:33 -0300

    Add publish test on delete.test

--- src/routes/__test__/delete.test.ts
+++ src/routes/__test__/delete.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
 import { Order, OrderStatus } from '../../models/order';
+import { natsWrapper } from '../../nats-wrapper';
 
 // On this course for this test the focus are the success case, but the other tests
 // may be implemented for you in same fashion we did for the other tests on
@@ -37,4 +38,29 @@ it('marks an order as cancelled', async () => {
 	expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
 });
 
-it.todo('emits a order cancelled event');
+it('emits a order cancelled event', async () => {
+	// Create a ticket with Ticket model
+	const ticket = Ticket.build({
+		title: 'Concert',
+		price: 20,
+	});
+	await ticket.save();
+
+	const user = global.signin();
+
+	// Make a request to create an order
+	const { body: order } = await request(app)
+		.post('/api/orders')
+		.set('Cookie', user)
+		.send({ ticketId: ticket.id })
+		.expect(201);
+
+	// Make a request to cancel the order
+	await request(app)
+		.delete(`/api/orders/${order.id}`)
+		.set('Cookie', user)
+		.send()
+		.expect(204);
+
+	expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
