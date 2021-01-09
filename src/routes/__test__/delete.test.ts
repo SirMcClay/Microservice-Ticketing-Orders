85ce97c8b837a4a3e26a3f2c9c263a0a0432b1f3
sirmcclay <mcclay.design@gmail.com>
2021-01-09 15:47:17 -0300

    Fix some tests that needs id property and fix two routes that needs version property

--- src/routes/__test__/delete.test.ts
+++ src/routes/__test__/delete.test.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
@@ -11,6 +12,7 @@ import { natsWrapper } from '../../nats-wrapper';
 it('marks an order as cancelled', async () => {
 	// Create a ticket with Ticket model
 	const ticket = Ticket.build({
+		id: mongoose.Types.ObjectId().toHexString(),
 		title: 'Concert',
 		price: 20,
 	});
@@ -41,6 +43,7 @@ it('marks an order as cancelled', async () => {
 it('emits a order cancelled event', async () => {
 	// Create a ticket with Ticket model
 	const ticket = Ticket.build({
+		id: mongoose.Types.ObjectId().toHexString(),
 		title: 'Concert',
 		price: 20,
 	});
