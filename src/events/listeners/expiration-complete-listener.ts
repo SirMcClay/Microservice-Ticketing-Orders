e8c8f4fee64dbb14c14755bd0965e6bdb5a483cb
sirmcclay <mcclay.design@gmail.com>
2021-01-13 09:40:16 -0300

    Logic to cancell the order changing status and - maybe - delete reference to ticket

--- src/events/listeners/expiration-complete-listener.ts
+++ src/events/listeners/expiration-complete-listener.ts
@@ -3,12 +3,25 @@ import {
 	Listener,
 	ExpirationCompleteEvent,
 	Subjects,
+	OrderStatus,
 } from '@sirmctickets/commontickets';
 import { queueGroupName } from './queue-group-name';
+import { Order } from '../../models/order';
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
 	readonly subject = Subjects.ExpirationComplete;
 	queueGroupName = queueGroupName;
 
-	async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {}
+	async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+		const order = await Order.findById(data.orderId);
+
+		if (!order) {
+			throw new Error('Order not found');
+		}
+
+		order.set({
+			status: OrderStatus.Cancelled,
+			ticket: null,
+		});
+	}
 }
